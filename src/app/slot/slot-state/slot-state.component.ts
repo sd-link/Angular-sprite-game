d3d3312919a8ba5bfa06f6f81ec51de2c92f68fc
Forever <lynk0613@hotmail.com>
2018-10-28 22:16:59 -0400

    add background

--- src/app/slot/slot-state/slot-state.component.ts
+++ src/app/slot/slot-state/slot-state.component.ts
@@ -13,6 +13,7 @@ export class SlotStateComponent implements OnInit {
   
   readyAssets: boolean;
   spriteRolling: any;
+  imgBackground: any;
 
   @ViewChild('wrapper') wrapper: ElementRef;
 
@@ -32,51 +33,37 @@ export class SlotStateComponent implements OnInit {
 
     PIXI.loader
     .add('assets/slots/sprites/dice-flow-sheet.json')
+    .add('background', 'assets/slots/pngs/background.png')
     .load((loader, resources) => {
       this.readyAssets = true;
-      
+
+      /* sprite */
       let frames = [];
-      ​
       for (let i = 0; i < 58; i++) {
         let val = i < 10 ? '0' + i : i;
   ​      frames.push(PIXI.Texture.fromFrame('Square_000' + val));
       }
-  ​
       this.spriteRolling = new PIXI.extras.AnimatedSprite(frames);
-
-      this.spriteRolling.anchor.set(0.5);
       this.spriteRolling.animationSpeed = 0.5;
       this.spriteRolling.play();
-  ​
+      this.spriteRolling.x = 0;
+      this.spriteRolling.y = 0;
       this.app.stage.addChild(this.spriteRolling);
-  ​
-      // Animate the rotation
-      this.app.ticker.add(() => {
-        this.spriteRolling.rotation += 0.01;
-      });
-    });
 
-    // PIXI.loader.add('bunny', 'assets/slots/sprites/dice-flow.png').load((loader, resources) => {
-    //   // This creates a texture from a 'bunny.png' image
-    //   const bunny = new PIXI.Sprite(resources.bunny.texture);
-  
-    //   // Setup the position of the bunny
-    //   bunny.x = this.app.renderer.width / 2;
-    //   bunny.y = this.app.renderer.height / 2;
-  
-    //   // Rotate around the center
-    //   bunny.anchor.x = 0.5;
-    //   bunny.anchor.y = 0.5;
-  
-    //   // Add the bunny to the scene we are building
-    //   this.app.stage.addChild(bunny);
-  
-    //   // Listen for frame updates
-    //   this.app.ticker.add(() => {
-    //     // each frame we spin the bunny around a bit
-    //     bunny.rotation += 0.01;
-    //   });
-    // });
+      /* background */
+      this.imgBackground = new PIXI.Sprite(resources.background.texture);
+      this.imgBackground.x = 0;
+      this.imgBackground.y = 0;
+
+      this.app.stage.addChild(this.imgBackground);
+
+  ​
+      
+      // Animate the rotation
+      // this.app.ticker.add(() => {
+      //   this.spriteRolling.rotation += 0.01;
+      // });
+    });
   }
 
 }
